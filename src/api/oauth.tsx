d66547267e85a1d7e0f23fce18a921ba81b7a637
ninew089 <jittanan.jck@gmail.com>
2022-06-10 23:47:58 +0700

    add

--- src/api/oauth.tsx
+++ src/api/oauth.tsx
@@ -26,8 +26,7 @@ export const discordGetAccessTokenApi = (params: URLSearchParams) => {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Access-Control-Allow-Origin': 'https://forwordle-ui.vercel.app',
       Accept: 'application/json',
-      "Access-Control-Allow-Headers": "Content-Type",
-      
+      'Access-Control-Allow-Headers': 'Content-Type',
     },
   });
 };
